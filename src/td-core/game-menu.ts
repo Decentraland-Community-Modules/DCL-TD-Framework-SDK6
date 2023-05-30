7424cb4001efe3dde39bdbe5cfdb1d835ff6eecd
TheCryptoTrader69 <91359820+TheCryptoTrader69@users.noreply.github.com>
2023-05-30 15:32:14 -0700

    Menu Update

--- src/td-core/game-menu.ts
+++ src/td-core/game-menu.ts
@@ -1366,32 +1366,33 @@ export class GameMenu
         //  button other scenes
         for(var i:number=0; i<this.stringsSceneNames.length; i++)
         {
-            const pos:Vector3 = new Vector3(((i%3)-1)*0.66,-0.075,0);
+            const index:number = i;
+            const pos:Vector3 = new Vector3(((index%3)-1)*0.66,-0.075,0);
             //  button creator object
-            this.menuGroupSceneInfo.AddMenuObject("buttonScene"+i, 5, "infoFrame");
-            this.menuGroupSceneInfo.AdjustMenuObject("buttonScene"+i, 0, pos);
-            this.menuGroupSceneInfo.AdjustMenuObject("buttonScene"+i, 1, new Vector3(0.22,0.18,0.2));
-            /*this.menuGroupSceneInfo.GetMenuObject("buttonScene"+i).addComponent
+            this.menuGroupSceneInfo.AddMenuObject("buttonScene"+index, 5, "infoFrame");
+            this.menuGroupSceneInfo.AdjustMenuObject("buttonScene"+index, 0, pos);
+            this.menuGroupSceneInfo.AdjustMenuObject("buttonScene"+index, 1, new Vector3(0.22,0.18,0.2));
+            this.menuGroupSceneInfo.GetMenuObject("buttonScene"+index).addComponent
             (
                 new OnPointerDown
                 (
                     (e) =>
                     {
-                    //open link
-                    openExternalURL("");
+                        //open link
+                        openExternalURL(this.stringsSceneURLs[index]);
                     },
                     {
                     button: ActionButton.ANY,
                     showFeedback: true,
-                    hoverText: "[E] CREATOR_NAME_"+i,
+                    hoverText: "[E] Join Map: "+this.stringsSceneNames[index],
                     distance: 8
                     }
                 )
-            );*/
+            );
             //  button creator text
-            this.menuGroupSceneInfo.AddMenuText("buttonScene"+i, "buttonText", this.stringsSceneNames[i]);
-            this.menuGroupSceneInfo.AdjustTextObject("buttonScene"+i, "buttonText", 0, new Vector3(0,0,-0.031));
-            this.menuGroupSceneInfo.AdjustTextObject("buttonScene"+i, "buttonText", 1, new Vector3(0.34,0.42,0.03));
+            this.menuGroupSceneInfo.AddMenuText("buttonScene"+index, "buttonText", this.stringsSceneNames[index]);
+            this.menuGroupSceneInfo.AdjustTextObject("buttonScene"+index, "buttonText", 0, new Vector3(0,0,-0.031));
+            this.menuGroupSceneInfo.AdjustTextObject("buttonScene"+index, "buttonText", 1, new Vector3(0.34,0.42,0.03));
         }
         //  button repo object
         this.menuGroupSceneInfo.AddMenuObject("buttonRepo", 5, "infoFrame");
@@ -1476,32 +1477,33 @@ export class GameMenu
         //generate creator linkages
         for(var i:number=0; i<this.stringsCreators.length; i++)
         {
-            const pos:Vector3 = new Vector3(((i%4)-1.5)*0.61,-0.38-(Math.round(i/8)*0.235),0);
+            const index:number = i;
+            const pos:Vector3 = new Vector3(((index%4)-1.5)*0.61,-0.38-(Math.round(index/8)*0.235),0);
             //  button creator object
-            this.menuGroupSceneInfo.AddMenuObject("buttonCreator"+i, 5, "creditFrame");
-            this.menuGroupSceneInfo.AdjustMenuObject("buttonCreator"+i, 0, pos);
-            this.menuGroupSceneInfo.AdjustMenuObject("buttonCreator"+i, 1, new Vector3(0.22,0.2,0.2));
-            /*this.menuGroupSceneInfo.GetMenuObject("buttonCreator"+i).addComponent
+            this.menuGroupSceneInfo.AddMenuObject("buttonCreator"+index, 5, "creditFrame");
+            this.menuGroupSceneInfo.AdjustMenuObject("buttonCreator"+index, 0, pos);
+            this.menuGroupSceneInfo.AdjustMenuObject("buttonCreator"+index, 1, new Vector3(0.22,0.2,0.2));
+            /*this.menuGroupSceneInfo.GetMenuObject("buttonCreator"+index).addComponent
             (
                 new OnPointerDown
                 (
                     (e) =>
                     {
-                    //open link
-                    openExternalURL("");
+                        //open link
+                        //openExternalURL();
                     },
                     {
                     button: ActionButton.ANY,
                     showFeedback: true,
-                    hoverText: "[E] CREATOR_NAME_"+i,
+                    hoverText: "[E] CREATOR_NAME_"+index,
                     distance: 8
                     }
                 )
             );*/
-            //  button creator text
-            this.menuGroupSceneInfo.AddMenuText("buttonCreator"+i, "buttonText", this.stringsCreators[i]);
-            this.menuGroupSceneInfo.AdjustTextObject("buttonCreator"+i, "buttonText", 0, new Vector3(0,0,-0.031));
-            this.menuGroupSceneInfo.AdjustTextObject("buttonCreator"+i, "buttonText", 1, new Vector3(0.30,0.30,0.03));
+            // button creator text
+            this.menuGroupSceneInfo.AddMenuText("buttonCreator"+index, "buttonText", this.stringsCreators[index]);
+            this.menuGroupSceneInfo.AdjustTextObject("buttonCreator"+index, "buttonText", 0, new Vector3(0,0,-0.031));
+            this.menuGroupSceneInfo.AdjustTextObject("buttonCreator"+index, "buttonText", 1, new Vector3(0.30,0.30,0.03));
         }
 
         //activate menu by default
@@ -1509,15 +1511,15 @@ export class GameMenu
     }
     private stringsSceneNames:string[] = 
     [
-        "TD Factory",
         "TD Shipyard",
+        "TD Factory",
         "TD Neon City",
     ];
     private stringsSceneURLs:string[] = 
     [
-        "//",
-        "//",
-        "//",
+        "https://play.decentraland.org/?realm=https%3A%2F%2Fworlds-content-server.decentraland.org%2Fworld%2FtdShipyard.dcl.eth",
+        "https://play.decentraland.org/?realm=https%3A%2F%2Fworlds-content-server.decentraland.org%2Fworld%2FtdFactory.dcl.eth",
+        "https://play.decentraland.org/?realm=https%3A%2F%2Fworlds-content-server.decentraland.org%2Fworld%2FtdCity.dcl.eth",
     ];
     private stringsCreators:string[] = 
     [
